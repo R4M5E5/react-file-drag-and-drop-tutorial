b94edaf3a055fd6ec6a5125aa68fd2436efb1c7f
Olli <58643307+R4M5E5@users.noreply.github.com>
2023-10-21 16:37:25 +0200

    Implement drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import "./App.css";
+import { useState } from "react";
+import { DragAndDropPad } from "./components/drag-and-drop";
+import { FileList } from "./components/file-list";
+import { Gallery } from "./components/gallery";
 
 function App() {
-    return <div className="app">Hello World</div>;
+  const [files, setFiles] = useState<Array<File>>([]);
+  const [imageUrls, setImageUrls] = useState<Array<string>>([]);
+
+  function onFilesDropped(newFiles: Array<File>) {
+    setFiles(newFiles);
+    const urls = newFiles
+      .filter((file) => file.type === "image/jpeg")
+      .map((file) => URL.createObjectURL(file));
+    setImageUrls(urls);
+  }
+
+  return (
+    <div className="app">
+      <DragAndDropPad onFilesDropped={onFilesDropped} className="drag-and-drop">
+        <p className="drop-hint">Drop files here</p>
+      </DragAndDropPad>
+      <FileList files={files} />
+      <Gallery imageUrls={imageUrls} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/drag-and-drop.tsx
@@ -0,0 +1,44 @@
+import { PropsWithChildren, DragEvent } from "react";
+type DragAndDropPadProps = {
+  className?: string;
+  onFilesDropped: (files: Array<File>) => void;
+};
+
+export const DragAndDropPad = ({
+  className,
+  children,
+  onFilesDropped,
+}: PropsWithChildren<DragAndDropPadProps>) => {
+  function onDrop(event: DragEvent<HTMLDivElement>) {
+    event.preventDefault();
+    const items = event.dataTransfer.items;
+    const files = event.dataTransfer.files;
+    if (items) {
+      processItems([...items]);
+    } else {
+      processFiles([...files]);
+    }
+  }
+
+  function processItems(items: Array<DataTransferItem>) {
+    const files = items
+      .filter((item) => item.kind === "file")
+      .map((item) => item.getAsFile())
+      .filter((file): file is File => file !== null);
+    processFiles(files);
+  }
+
+  function processFiles(files: Array<File>) {
+    onFilesDropped(files);
+  }
+
+  function onDragOver(event: DragEvent<HTMLDivElement>) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className={className} onDragOver={onDragOver} onDrop={onDrop}>
+      {children}
+    </div>
+  );
+};
